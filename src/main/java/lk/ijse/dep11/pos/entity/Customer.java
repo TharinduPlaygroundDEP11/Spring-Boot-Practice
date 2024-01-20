66635b794eaa8d5f53dfd7209b9fbf461a7e3ff1
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-01-20 15:48:26 +0530

    Create Customer Entity

---
+++ src/main/java/lk/ijse/dep11/pos/entity/Customer.java
@@ -0,0 +1,115 @@
+package lk.ijse.dep11.pos.entity;
+
+import com.vladmihalcea.hibernate.type.json.JsonType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "customers")
+@TypeDefs({
+        @TypeDef(name = "json", typeClass = JsonType.class)
+})
+public class Customer implements Serializable {
+    @Id
+    @Column(name = "customer_id", length = 20)
+    private int customerId;
+
+    @Column(name = "customer_name", length = 150, nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_address")
+    private String customerAddress;
+
+    @Type(type = "json")
+    @Column(name = "contact_numbers", columnDefinition = "json")
+    private ArrayList contactNumbers;
+
+    @Column(name = "nic")
+    private String nic;
+
+    @Column(name = "active_status", columnDefinition = "TINYINT default 0")
+    private boolean activeStatus;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String customerName, String customerAddress, ArrayList contactNumbers, String nic, boolean activeStatus) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.contactNumbers = contactNumbers;
+        this.nic = nic;
+        this.activeStatus = activeStatus;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public ArrayList getContactNumbers() {
+        return contactNumbers;
+    }
+
+    public void setContactNumbers(ArrayList contactNumbers) {
+        this.contactNumbers = contactNumbers;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public boolean isActiveStatus() {
+        return activeStatus;
+    }
+
+    public void setActiveStatus(boolean activeStatus) {
+        this.activeStatus = activeStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", contactNumbers=" + contactNumbers +
+                ", nic='" + nic + '\'' +
+                ", activeStatus=" + activeStatus +
+                '}';
+    }
+}
