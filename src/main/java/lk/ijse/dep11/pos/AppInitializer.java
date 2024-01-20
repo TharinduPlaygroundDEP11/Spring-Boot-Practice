f347e8b785f161f6eb345d438b7b414cd0f1fa15
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-01-20 11:23:49 +0530

    Initial Commit

---
+++ src/main/java/lk/ijse/dep11/pos/AppInitializer.java
@@ -0,0 +1,11 @@
+package lk.ijse.dep11.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(AppInitializer.class, args);
+    }
+}
